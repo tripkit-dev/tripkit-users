23049199b9220069b14da9893e427d11e6d1cffe
신정아 <jeonga@khu.ac.kr>
2022-12-21 19:12:00 +0900

    feat : test-service 2

---
+++ test-server2/src/main/java/tripkit/testserver2/TestServer2Application.java
@@ -0,0 +1,13 @@
+package tripkit.testserver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestServer2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestServer2Application.class, args);
+  }
+
+}
---
+++ test-server2/src/test/java/tripkit/testserver2/TestServer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package tripkit.testserver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestServer2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
