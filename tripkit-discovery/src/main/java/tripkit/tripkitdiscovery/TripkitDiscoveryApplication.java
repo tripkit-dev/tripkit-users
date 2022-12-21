8592d4fd67713c5e16cb6f395a4d6e3ed8dafd5b
신정아 <jeonga@khu.ac.kr>
2022-12-21 19:12:52 +0900

    chore : (euureka) discovery server for MSA

---
+++ tripkit-discovery/src/main/java/tripkit/tripkitdiscovery/TripkitDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package tripkit.tripkitdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class TripkitDiscoveryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TripkitDiscoveryApplication.class, args);
+  }
+
+}
---
+++ tripkit-discovery/src/test/java/tripkit/tripkitdiscovery/TripkitDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package tripkit.tripkitdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripkitDiscoveryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
