6f19661e46466ebeeb94b62ffd2d1619862796ef
신정아 <jeonga@khu.ac.kr>
2022-12-21 19:14:16 +0900

    init: gateway server init

---
+++ tripkit-gateway/src/main/java/tripkit/tripkitgateway/TripkitGatewayApplication.java
@@ -0,0 +1,13 @@
+package tripkit.tripkitgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripkitGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TripkitGatewayApplication.class, args);
+  }
+
+}
---
+++ tripkit-gateway/src/test/java/tripkit/tripkitgateway/TripkitGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package tripkit.tripkitgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripkitGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
