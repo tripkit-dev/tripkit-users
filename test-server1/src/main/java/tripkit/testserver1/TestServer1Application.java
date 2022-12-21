b2b6ffe33c61be82b989e622ba27f6e84bbc57a3
신정아 <jeonga@khu.ac.kr>
2022-12-21 19:11:39 +0900

    feat : tripkit test-server1

---
+++ test-server1/src/main/java/tripkit/testserver1/TestServer1Application.java
@@ -0,0 +1,13 @@
+package tripkit.testserver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestServer1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestServer1Application.class, args);
+  }
+
+}
---
+++ test-server1/src/test/java/tripkit/testserver1/TestServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tripkit.testserver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestServer1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
