e6d933200c5bbddd961ac387aae1f5a5b76e7475
JeongA-Shin <jeonga@khu.ac.kr>
2023-01-15 22:05:01 +0900

    게이트웨이를 통한 요청도 csrf를 비활성화 시키기 위해서 SecurityWebFilterChain 빈을 등록

--- tripkit-gateway/src/main/java/tripkit/tripkitgateway/TripkitGatewayApplication.java
+++ tripkit-gateway/src/main/java/tripkit/tripkitgateway/TripkitGatewayApplication.java
@@ -2,6 +2,9 @@ package tripkit.tripkitgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 public class TripkitGatewayApplication {
@@ -9,5 +12,16 @@ public class TripkitGatewayApplication {
   public static void main(String[] args) {
     SpringApplication.run(TripkitGatewayApplication.class, args);
   }
+
+  /**
+   * 게이트웨이를 통한 요청도 csrf를 비활성화 시키기 위해서 SecurityWebFilterChain 빈을 등록
+   * @param security
+   * @return
+   */
+  @Bean
+  public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity security) {
+    return security.csrf().disable().build();
+  }
+
 
 }
